f1be5be917f3fb9cdf354e3df87ed92c987f4cf1
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-11-29 14:02:03 -0300

    começandoa adicionar o javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let input1=document.getElementById()
+function adicionar(){
+    const dvExterior=document.createElemet('div')
+    dvExterior.classList.add('dv-exterior')
+    const dvInterior=document.createElement('div')
+    dvInterior.classList.add('dv-interior')
+    const paragrafo=document.createElement('p'
+        paragrafo.textContent=valorInput1.value
+    )
+}
