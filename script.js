8e3fc88806350ec1719832e7f871f6223c551ffc
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-11-29 17:45:59 -0300

    finalizando o JavaScript do board

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-let input1=document.getElementById()
-function adicionar(){
-    const dvExterior=document.createElemet('div')
-    dvExterior.classList.add('dv-exterior')
-    const dvInterior=document.createElement('div')
-    dvInterior.classList.add('dv-interior')
-    const paragrafo=document.createElement('p'
-        paragrafo.textContent=valorInput1.value
-    )
-}
+let input1=document.getElementById('input-1')
+let input2=document.getElementById('input-2')
+let box3=document.getElementById('box-3')
+let adicionar=document.getElementById('adicionar')
+let totalTarefas=0
+let contador=document.getElementById('contador')
+adicionar.addEventListener('click',()=>{
+    if(input1.value===''||input2.value===''){
+        alert('Por favor, preencha todos os campos.')
+    }else{
+        let dvExterior=document.createElement('div')
+        dvExterior.classList.add('dv-exterior')
+
+    let dvInterior=document.createElement('div')
+        dvInterior.classList.add('dv-interior')
+
+    let paragrafo=document.createElement('p')
+        paragrafo.classList.add('tarefa')
+        paragrafo.textContent=input1.value
+
+    let dvDataEtiqueta=document.createElement('div')
+        dvDataEtiqueta.classList.add('dt-et')
+
+    let etiqueta=document.createElement('p')
+        etiqueta.classList.add('etiqueta')
+        etiqueta.textContent=input2.value
+
+    let data=document.createElement('p')
+        data.classList.add('data')
+        data.textContent='Criado em:'
+
+    let conclua=document.createElement('button')
+        conclua.classList.add('concluir')
+        conclua.textContent='Concluir'
+
+        box3.appendChild(dvExterior)
+        dvExterior.appendChild(dvInterior)
+        dvExterior.appendChild(conclua)
+        dvInterior.appendChild(paragrafo)
+        dvInterior.appendChild(dvDataEtiqueta)
+        dvDataEtiqueta.appendChild(etiqueta)
+        etiqueta.textContent=''
+
+        dvDataEtiqueta.appendChild(data)
+
+    totalTarefas++
+    contador.innerText=totalTarefas.toString()
+
+    input1.value=''
+    input2.value=''
+    }
+})
